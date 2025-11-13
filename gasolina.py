f67eb21e50ad094518359129d1fc29a7b57caa00
joykrn <octajoxk@gmail.com>
2025-11-13 00:24:07 +0000

    gasolina.csv

---
+++ gasolina.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('gasolina.csv')
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='dia', y='venda', data=df, marker='o', color='blue')
+
+plt.title('Preço da Gasolina por Dia')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+
+plt.savefig('gasolina.png')
+
+plt.show()
